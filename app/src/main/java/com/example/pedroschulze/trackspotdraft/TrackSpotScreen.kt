a5aa72d1bb9e0a142d1c78b773ba4cca27521133
pedroschulze <pedscn@gmail.com>
2018-11-06 17:05:29 +0000

    Working viewing spots, with thumbnails, paths and spot name. Issues: -Very slow loading, need to find a way to make images load faster or make it an async task. -Memory leaks, app crashed when view spots is clicked a few times, likely due to glide. -Files need to be deleted from temp. -ugly code
    Got swipe library to work android

--- app/src/main/java/com/example/pedroschulze/trackspotdraft/TrackSpotScreen.kt
+++ app/src/main/java/com/example/pedroschulze/trackspotdraft/TrackSpotScreen.kt
@@ -7,6 +7,12 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 
 import kotlinx.android.synthetic.main.activity_track_spot_screen.*
+import swipeable.com.layoutmanager.OnItemSwiped
+import swipeable.com.layoutmanager.SwipeableTouchHelperCallback
+import android.support.v7.widget.AppCompatButton
+import swipeable.com.layoutmanager.SwipeableLayoutManager
+import swipeable.com.layoutmanager.touchelper.ItemTouchHelper
+
 
 class TrackSpotScreen : AppCompatActivity() {
 
@@ -22,6 +28,51 @@ class TrackSpotScreen : AppCompatActivity() {
         Log.e("spotPath", spotPath)
         Log.e("spotPicname", spotPicname)
         Log.e("limb", limb)
+
+        var adapter = ListAdapter()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val swipeableTouchHelperCallback = object : SwipeableTouchHelperCallback(object : OnItemSwiped {
+            override fun onItemSwiped() {
+                adapter.removeTopItem()
+            }
+
+            override fun onItemSwipedLeft() {
+                Log.e("SWIPE", "LEFT")
+            }
+
+            override fun onItemSwipedRight() {
+                Log.e("SWIPE", "RIGHT")
+            }
+
+            override fun onItemSwipedUp() {
+                Log.e("SWIPE", "UP")
+            }
+
+            override fun onItemSwipedDown() {
+                Log.e("SWIPE", "DOWN")
+            }
+        }) {
+            override fun getAllowedSwipeDirectionsMovementFlags(viewHolder: RecyclerView.ViewHolder): Int {
+                return ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeableTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+        recyclerView.layoutManager = SwipeableLayoutManager().setAngle(10)
+                .setAnimationDuratuion(450)
+                .setMaxShowCount(3)
+                .setScaleGap(0.1f)
+                .setTransYGap(0)
+        adapter = ListAdapter()
+        recyclerView.adapter = adapter
+
+    //    val button = findViewById(R.id.swipe)
+    //    button.setOnClickListener(object : View.OnClickListener() {
+    //        fun onClick(v: View) {
+    //            itemTouchHelper.swipe(recyclerView.findViewHolderForAdapterPosition(0), ItemTouchHelper.DOWN)
+    //        }
+    //    })
 
     }
 
